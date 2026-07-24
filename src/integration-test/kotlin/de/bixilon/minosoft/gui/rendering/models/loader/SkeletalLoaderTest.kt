fff5838a10b77630cc6a0ab3b06796036245560d
Jacob Repp <jacobrepp@gmail.com>
2026-07-24 10:38:57 -1000

    test(rendering): cover transactional content reload

--- src/integration-test/kotlin/de/bixilon/minosoft/gui/rendering/models/loader/SkeletalLoaderTest.kt
+++ src/integration-test/kotlin/de/bixilon/minosoft/gui/rendering/models/loader/SkeletalLoaderTest.kt
@@ -1,6 +1,7 @@
 /*
  * Minosoft
  * Copyright (C) 2020-2025 Moritz Zwerger
+ * Copyright (C) 2026 Jacob Repp
  *
  * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
  *
@@ -20,18 +21,29 @@ import de.bixilon.kutil.reflection.ReflectionUtil.forceSet
 import de.bixilon.kutil.reflection.ReflectionUtil.getFieldOrNull
 import de.bixilon.kutil.stream.InputStreamUtil.readAll
 import de.bixilon.minosoft.assets.MemoryAssetsManager
+import de.bixilon.minosoft.assets.model.generation.ContentFidelityLoader
+import de.bixilon.minosoft.assets.model.skeletal.SkeletalContentParsers
+import de.bixilon.minosoft.assets.model.skeletal.cem.CEM_PARSER_REGISTRATION
+import de.bixilon.minosoft.assets.session.SessionDataPackManager
 import de.bixilon.minosoft.data.registries.identified.Namespaces.minosoft
 import de.bixilon.minosoft.data.registries.identified.ResourceLocation
 import de.bixilon.minosoft.gui.rendering.RenderContext
+import de.bixilon.minosoft.gui.rendering.framebuffer.FramebufferManager
+import de.bixilon.minosoft.gui.rendering.framebuffer.world.WorldFramebuffer
 import de.bixilon.minosoft.gui.rendering.models.loader.SkeletalLoader.Companion.sModel
 import de.bixilon.minosoft.gui.rendering.skeletal.baked.BakedSkeletalTransform
 import de.bixilon.minosoft.gui.rendering.skeletal.model.SkeletalModel
 import de.bixilon.minosoft.gui.rendering.system.dummy.DummyRenderSystem
 import de.bixilon.minosoft.gui.rendering.system.dummy.texture.DummyTexture
 import de.bixilon.minosoft.gui.rendering.system.dummy.texture.DummyTextureManager
+import de.bixilon.minosoft.gui.rendering.system.dummy.texture.DummyTextureLoader
+import de.bixilon.minosoft.gui.rendering.renderer.renderer.RendererManager
 import de.bixilon.minosoft.protocol.network.session.play.SessionTestUtil
 import de.bixilon.minosoft.test.ITUtil.allocate
 import org.testng.Assert.assertEquals
+import org.testng.Assert.assertNotSame
+import org.testng.Assert.assertSame
+import org.testng.Assert.assertThrows
 import org.testng.annotations.Test
 
 @Test(groups = ["skeletal"])
@@ -42,6 +54,7 @@ class SkeletalLoaderTest {
     private fun createContext(): RenderContext {
         val context = RenderContext::class.java.allocate()
         context::session.forceSet(SessionTestUtil.createSession())
+        context::profile.forceSet(context.session.profiles.rendering)
         context::system.forceSet(DummyRenderSystem(context))
         context::textures.forceSet(DummyTextureManager(context))
 
@@ -88,5 +101,72 @@ class SkeletalLoaderTest {
 
 
         assertEquals(baked.transform.children, mapOf("body" to BakedSkeletalTransform(1, Vec3f(0.0f, 0.5f, 0.0f), mapOf("head" to BakedSkeletalTransform(2, Vec3f(0.0f, 1.0f, 0.0f), emptyMap())))))
+    }
+
+    fun `content reload swaps uploaded models and rejects missing texture candidate`() {
+        val parser = SkeletalContentParsers.register(CEM_PARSER_REGISTRATION)
+        val context = createContext()
+        val session = context.session
+        val dataPacks = SessionDataPackManager()
+        session::dataPacks.forceSet(dataPacks)
+        context::thread.forceSet(Thread.currentThread())
+        val framebuffer = FramebufferManager::class.java.allocate()
+        framebuffer::world.forceSet(WorldFramebuffer::class.java.allocate())
+        context::framebuffer.forceSet(framebuffer)
+        context::renderer.forceSet(RendererManager(context))
+        val modelLoader = ModelLoader(context)
+        context::models.forceSet(modelLoader)
+        val source = ResourceLocation.of("test:optifine/cem/zombie.jem")
+        val texture = ResourceLocation.of("test:textures/__content/zombie.png")
+        val assets = MemoryAssetsManager()
+        session.assets.add(assets)
+        context.textures.static.create(texture, loader = DummyTextureLoader)
+        assets.push(source, cem(4))
+
+        try {
+            session.contentFidelity.reload {
+                ContentFidelityLoader(session.assets, dataPacks).prepare()
+            }
+            modelLoader.skeletal.registerContentFidelity()
+            val entity = session.contentFidelity.lease()!!.use { lease ->
+                val content = lease.value.skeletal.values.flatten().single()
+                ResourceLocation(content.source.namespace, content.source.path.substringAfterLast('/').substringBeforeLast('.'))
+            }
+            val modelName = requireNotNull(modelLoader.skeletal.contentModel(entity))
+            modelLoader.skeletal.load(SimpleLatch(0))
+            context.textures.static.load(SimpleLatch(0))
+            context.textures.static.upload(SimpleLatch(0))
+            modelLoader.skeletal.bake(SimpleLatch(0))
+            modelLoader.skeletal.upload()
+
+            val first = modelLoader.skeletal[modelName]!!
+            val retained = first.createInstance(context)
+            retained.load()
+
+            assets.push(source, cem(6))
+            assertEquals(modelLoader.skeletal.reloadContentFidelity(), 2L)
+            val second = modelLoader.skeletal[modelName]!!
+            assertNotSame(second, first)
+            assertEquals(modelLoader.skeletal.contentModel(entity), modelName)
+            assertThrows(IllegalStateException::class.java) { first.createInstance(context) }
+
+            assets.push(source, cem(8, texture = "textures/entity/new.png"))
+            assertThrows(IllegalArgumentException::class.java) {
+                modelLoader.skeletal.reloadContentFidelity()
+            }
+            assertEquals(session.contentFidelity.generationId, 2L)
+            assertSame(modelLoader.skeletal[modelName], second)
+
+            retained.unload()
+            modelLoader.skeletal.unload()
+        } finally {
+            session.contentFidelity.close()
+            parser.close()
+        }
+    }
+
+    private fun cem(size: Int, texture: String? = null): String {
+        val rootTexture = texture?.let { ",\"texture\":\"$it\"" }.orEmpty()
+        return """{"textureSize":[64,64]$rootTexture,"models":[{"id":"body","part":"body","boxes":[{"coordinates":[0,0,0,$size,$size,$size],"textureOffset":[0,0]}]}]}"""
     }
 }
